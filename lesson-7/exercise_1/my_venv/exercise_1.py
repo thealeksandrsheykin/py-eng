cb8aaf102b1e3dc8be46772058ffa7059c30cda3
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-01-10 18:44:36 +0300

    Modify exercise_1.py

--- lesson-7/exercise_1/my_venv/exercise_1.py
+++ lesson-7/exercise_1/my_venv/exercise_1.py
@@ -10,5 +10,17 @@ Next-Hop 10.0.13.3
 Last update 3d18h
 Outbound Interface FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
-
 """
+array = list()
+with open (r'ospf.txt', 'r') as file:
+    for line in file:
+        line = line.rstrip()
+        line = line.replace('[','').replace(']','')
+        line = line.replace('via','').replace(',','')
+        array = line.split()[1:]
+        print(f'Prefix {array[0]}\n'
+              f'AD/Metric {array[1]}\n'
+              f'Next-Hop {array[2]}\n'
+              f'Last update {array[3]}\n'
+              f'Outbound Interface {array[4]}\n')
+
