db696c0f06275f2442d2480ed6ae101d57ac1de2
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-01-31 19:34:59 +0300

    Modify exercise_6.py

--- lesson-9/exercise_6/my_venv/exercise_6.py
+++ lesson-9/exercise_6/my_venv/exercise_6.py
@@ -12,3 +12,30 @@
 Проверить работу функции на примере файла config_sw2.txt
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+def get_int_vlan_map(config_filename):
+    """
+    :param config_filename: имя конфигурационного файла
+    :return: возвращает кортеж из двух словарей
+    """
+    access_dict = dict()
+    trunk_dict  = dict()
+    with open (config_filename) as file:
+        for line in file:
+            line = line.rstrip()
+            if line.startswith('interface FastEthernet'):
+                _,intf = line.split()
+                access_dict[intf] = '1'
+            elif 'access vlan' in line:
+                vlan = line.split()[-1]
+                access_dict[intf] = vlan
+            elif 'allowed vlan' in line:
+                vlans = line.split()[-1]
+                trunk_dict[intf] = vlans.split(',')
+                del access_dict[intf]
+            else: continue
+    return (access_dict,trunk_dict)
+
+if __name__ == '__main__':
+    filename = 'config_sw2.txt'
+    print(get_int_vlan_map(filename))
