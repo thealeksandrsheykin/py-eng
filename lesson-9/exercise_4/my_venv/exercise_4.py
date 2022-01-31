b6b5bf7e3857212f480da0c9b4e6093526d8bae9
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-01-31 17:56:47 +0300

    Modify exercise_4.py

--- lesson-9/exercise_4/my_venv/exercise_4.py
+++ lesson-9/exercise_4/my_venv/exercise_4.py
@@ -10,3 +10,39 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    """
+    :param   intf_vlan_mapping: словарь с соответствием интерфейс-VLANы
+    :param   trunk_template: шаблон конфигурации trunk-портов в виде списка команд
+    :return: словарь:
+                • ключи: имена интерфейсов, вида «FastEthernet0/1»
+                • значения: список команд, который надо выполнить на этом интерфейсе
+    """
+
+    my_dict = dict()
+    for intf,vlans in trunk_config.items():
+        my_list = list()
+        for line in trunk_template:
+            if line.endswith('allowed vlan'):
+                my_list.append(f'{line} {",".join([str(j) for j in vlans])}')
+            else:
+                my_list.append(line)
+        my_dict[intf] = my_list
+    return my_dict
+
+
+
+if __name__ == '__main__':
+
+    trunk_mode_template = [
+        "switchport mode trunk", "switchport trunk native vlan 999",
+        "switchport trunk allowed vlan"
+    ]
+
+    trunk_config = {
+        "FastEthernet0/1": [10, 20, 30],
+        "FastEthernet0/2": [11, 30],
+        "FastEthernet0/4": [17]
+    }
+    print(generate_trunk_config(trunk_config,trunk_mode_template))
