d38cee2e7fe1482d9fd09ce0dce2b485a1ad91ae
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-04-06 12:09:43 +0300

    Modify exercise-2.py

--- lesson-11/exercise-2/my_venv/exercise-2.py
+++ lesson-11/exercise-2/my_venv/exercise-2.py
@@ -37,8 +37,16 @@ def create_network_map(filenames):
     :param filenames: список с именами файлов, в которых находится вывод команды show cdp neighbors
     :return: словарь, который описывает соединения между устройствами
     """
+    my_dict = dict()
+    for filename in filenames:
+        with open(filename) as file:
+           my_dict |= parse_cdp_neighbors(file.read()) # python 3.9
+           # my_dict.update(parse_cdp_neighbors(file.read()))
+    return my_dict
+
 
 
 if __name__ == '__main__':
     infiles = ["sh_cdp_n_sw1.txt","sh_cdp_n_r1.txt","sh_cdp_n_r2.txt","sh_cdp_n_r3.txt"]
-    create_network_map(infiles)
+    for i,j in create_network_map(infiles).items():
+        print(f' Device {i[0]}({i[1]}) --> Device {j[0]}({j[1]})')
