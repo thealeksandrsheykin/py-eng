a8b39770cc8b347253fc1dff8b6a014da7ad1ac5
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-05-12 18:12:08 +0300

    Modify exercise-3.py

--- lesson-12/exercise-3/my_venv/exercise-3.py
+++ lesson-12/exercise-3/my_venv/exercise-3.py
@@ -16,6 +16,7 @@
 from itertools import zip_longest
 from tabulate import tabulate
 
+#  Способ №1
 def print_ip_table(reach, unreach):
     '''
     Функция отображает таблицу доступных и недоступных IPадресов
@@ -27,7 +28,16 @@ def print_ip_table(reach, unreach):
     combined_list = list(zip_longest(reach,unreach))
     print(tabulate(combined_list,headers=headers,tablefmt='grid',stralign='center'))
 
+# Способ №2
+def print_ip_table_2(reach,unreach):
+    my_dict = {'Reachable':reach, 'Unreachable':unreach}
+    print(tabulate(my_dict,headers='keys',tablefmt='pipe',stralign='center'))
+
+
 if __name__ == '__main__':
     reach = ['10.1.1.1','10.1.1.2','10.1.1.9']
     unreach = ['10.1.1.7','10.1.1.8']
+    print('Способ №1:')
     print_ip_table(reach,unreach)
+    print('Способ №2:')
+    print_ip_table_2(reach, unreach)
