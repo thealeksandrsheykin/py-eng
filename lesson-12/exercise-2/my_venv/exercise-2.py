f65a862090dfa5fa969dc77c0d0ee164316e62d2
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-05-12 17:38:29 +0300

    Modify exercise-2.py

--- lesson-12/exercise-2/my_venv/exercise-2.py
+++ lesson-12/exercise-2/my_venv/exercise-2.py
@@ -19,12 +19,32 @@ IP-адресов.Элементы списка могут быть в форм�
     '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 '''
 
-def convert_ranges_to_ip_list(ip_addresses):
+import ipaddress
+from pprint import pprint
+
+def convert_ranges_to_ip_list(list_addresses):
     '''
     Функция конвертирует список IP-адресов в разных форматах в список, где каждый IP-адрес указан отдельно.
     :param ip_addresses: список IP-адресов и/или диапазон IP-адресов
     :return: список IP-адресов
     '''
+    result = list()
+    for address in list_addresses:
+        if '-' in address:
+            start_address,end_address = address.split('-')
+            ip_1 = ipaddress.ip_address(start_address)
+            if end_address.isdigit():
+                new_address = '.'.join(start_address.split('.')[:-1]) + '.' + end_address
+                ip_2 = ipaddress.ip_address(new_address)
+            else:
+                ip_2 = ipaddress.ip_address(end_address)
+            while ip_1 <= ip_2:
+                result.append(str(ip_1))
+                ip_1 = ip_1 + 1
+        else:
+            result.append(address)
+    return result
 
 if __name__ == '__main__':
     my_list = ['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']
+    pprint(convert_ranges_to_ip_list(my_list))
