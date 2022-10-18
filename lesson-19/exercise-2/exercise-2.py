92ef35186df1423c953aa184fb9c1c7b6dfd55f7
Aleksandr <alexander_sheykin@mail.ru>
2022-10-18 15:23:44 +0300

    lesson-19(exercise-2) initial commit.

---
+++ lesson-19/exercise-2/exercise-2.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+
+"""
+Создать функцию send_show_command_to_devices, которая отправляет одну и ту же команду show на разные устройства в
+параллельных потоках, а затем записывает вывод команд в файл. Вывод с устройств в файле может быть в любом порядке.
+Параметры функции:
+    • devices  - список словарей с параметрами подключения к устройствам
+    • command  - команда
+    • filename - имя текстового файла, в который будут записаны выводы всех команд
+    • limit    - максимальное количество параллельных потоков (по умолчанию 3)
+Функция ничего не возвращает. Вывод команд должен быть записан в обычный текстовый файл в таком формате (перед вы-
+водом команды надо написать имя хоста и саму команду):
+
+R1#sh ip int br
+Interface       IP-Address      OK?     Method  Status  Protocol
+Ethernet0/0     192.168.100.1   YES     NVRAM   up      up
+Ethernet0/1     192.168.200.1   YES     NVRAM   up      up
+
+R2#sh ip int br
+Interface       IP-Address      OK?     Method  Status                Protocol
+Ethernet0/0     192.168.100.2   YES     NVRAM   up                    up
+Ethernet0/1     10.1.1.1        YES     NVRAM   administratively down down
+
+R3#sh ip int br
+Interface       IP-Address      OK?     Method  Status                Protocol
+Ethernet0/0     192.168.100.3   YES     NVRAM   up                    up
+Ethernet0/1     unassigned      YES     NVRAM   administratively down down
+
+Для выполнения задания можно создавать любые дополнительные функции.
+Проверить работу функции на устройствах из файла devices.yaml
+
+"""
+
+import yaml
+from concurrent.futures import ThreadPoolExecutor
+from netmiko import (ConnectHandler, NetmikoTimeoutException, NetmikoBaseException, NetMikoAuthenticationException)
+
+
+def send_show_command_to_devices(devises: list, command: str, filename: str = 'result.txt', limit: int = 3) -> any:
+    """
+    The function parallel sent one command to different devices adn write result to text file.
+    :param devises: list of dict with parameters connection to devices
+    :param command: command
+    :param filename: the name of the text file to write the result
+    :param limit: max amount thread
+    :return: None
+    """
+    ...
+
+
+if __name__ == '__main__':
+    command = 'sh ip int br'
+    with open('devices.yaml', 'r') as file:
+        devices = yaml.safe_load(file)
+    send_show_command_to_devices(devices, command)
