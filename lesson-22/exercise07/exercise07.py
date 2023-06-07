35170bd800f4626e54dba74c8f929bb2b37337e1
Aleksandr Sheykin <alexander_sheykin@mail.ru>
2023-06-07 23:20:06 +0300

    Lesson #22 exercise07

---
+++ lesson-22/exercise07/exercise07.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+import telnetlib, time
+
+class CiscoTelnet:
+    def __init__(self, ip: str, username: str, password: str, secret: str) -> None:
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.secret = secret
+        self.telnet = telnetlib.Telnet(self.ip)
+
+        self.telnet.read_until(b'Username')
+        self.telnet.write(self._write_line(self.username))
+        self.telnet.read_until(b'Password')
+        self.telnet.write(self._write_line(self.password))
+        flag, _, _ = self.telnet.expect([b'>', b'#'])
+        if not flag:
+            self.telnet.write(b'enable\n')
+            self.telnet.read_until(b'Password')
+            self.telnet.write(self._write_line(self.secret))
+            self.telnet.read_until(b'#', timeout=5)
+        self.telnet.write(b'terminal length 0\n')
+        self.telnet.read_until(b'#', timeout=5)
+        time.sleep(5)
+
+    def _write_line(self, line: str) -> str:
+        return f'{line}\n'.encode('utf-8')
+
+    def send_show_command(self, command: str,
+                          parse: bool = True,
+                          templates: str = 'templates',
+                          index: str = 'index') -> str:
+        self.telnet.write(self._write_line(command))
+        return self.telnet.read_until(b'#').decode('utf-8')
+
+
+if __name__ == '__main__':
+    sw1 = {
+        'ip': '192.168.100.1',
+        'username': 'admin',
+        'password': 'cisco',
+        'secret': 'cisco'}
+    device = CiscoTelnet(**sw1)
+    print(device.send_show_command('sh ip int br'))
