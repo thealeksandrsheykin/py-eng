759f081d03067f7a1e218e64fca2dc640d2e6e64
ASheykin <alexander_sheykin@mail.ru>
2023-06-26 18:43:05 +0300

    Lesson #25 exercise01 is Done!

---
+++ lesson-25/exercise01/create_db.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+
+import os
+import sqlite3
+
+DB_NAME = 'dhcp_snooping.db'
+
+
+def create_db(dbname):
+    if not os.path.exists(dbname):
+        print(f'Создаю базу данных {dbname}...')
+        connect = sqlite3.connect(dbname)
+    else:
+        print(f'База данных {dbname} существует...')
+        return 0
+    with open(r'dhcp_snooping_schema.sql', 'r') as file:
+        schema = file.read()
+    connect.executescript(schema)
+    connect.commit()
+    connect.close()
+
+
+if __name__ == '__main__':
+    create_db(DB_NAME)
